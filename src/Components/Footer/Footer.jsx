5dd7b33bedc4a19639edef4f670b5c90b1ae7e72
itsAman-24 <amanpan2410@gmail.com>
2024-06-26 10:00:02 +0530

    Added

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <div className={classes.footer}>
+    <p>Copyright©2024 All rights reserved. Made by <span>&nbsp;Aman Kumar Pandey 2024</span></p>
+  </div>
+  )
+}
+
+export default Footer
